14887e34d991784cafe46e0648da93d94c59a263
owolabi-develop <owolabidevelop84@gmail.com>
2023-01-14 13:04:01 -0800

    initial commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("ServerlessDoc/",include("serverlessDoc.urls"))
+    path("",include("serverlessDoc.urls"))
 ]
